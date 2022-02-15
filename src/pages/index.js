c8ff32836ddb3fc0a59aad26469fac5f99e634fa
Jihoon LEE <82790390+hnc-jihoon@users.noreply.github.com>
2022-02-15 16:27:52 +0900

    Change a sample image
    - and replace the lockfile with yarn.lock

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,11 +12,11 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
-      src="../images/gatsby-astronaut.png"
+      src="../images/cat.jpg"
       width={300}
       quality={95}
       formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
+      alt="A cat"
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
